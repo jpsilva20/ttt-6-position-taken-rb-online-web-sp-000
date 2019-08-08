8dbb3e51aaad5e7cc1f5b97ea14009448171f398
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-08 21:28:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,8 @@
 # code your #position_taken? method here!
 
-def position_taken?(board,index)
-  if board[index] == " " or board[index] == "" or board[index] == nil 
+def position_taken?(board, index)
+  if (board[index] == " " or board[index] == "" or board[index] == nil) 
     return false
-  end 
   else 
     return true 
-  end
 end 
